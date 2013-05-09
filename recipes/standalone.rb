9a7a2a5af9d51dfabd60fc3e52ea0923c90649c0
Ellis Percival <ellis.percival@imagination.com>
2013-05-09 20:11:54 +0100

    Added autofs::standalone recipe

---
+++ recipes/standalone.rb
@@ -0,0 +1,29 @@
+#
+# Cookbook Name:: autofs
+# Recipe:: standalone
+#
+# Copyright 2013, Failcode
+#
+# All rights reserved - Do Not Redistribute
+#
+
+include_recipe "autofs"
+
+template "/etc/auto.master" do
+  source "auto.master.erb"
+  owner "root"
+  group "root"
+  mode "0644"
+  notifies :reload, resources(:service => "autofs"), :immediately
+end
+
+node[:autofs][:maps].each do |map, args|
+  template args[:source].gsub(/file:/, '') do
+    owner "root"
+    group "root"
+    mode 0644
+    source "auto.map.erb"
+    variables(:keys => args[:keys])
+    notifies :reload, resources(:service => "autofs"), :immediately
+  end
+end

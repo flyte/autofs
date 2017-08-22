adfda0476e60bf6ddcd9a7592086e224be23557d
Goncalo Gomes <goncalo.gomes@workday.com>
2017-08-22 12:41:53 +0100

    reduce dependency on non-mandatory packages
    The dependencies on the network filesystems to be supported
    should be separate from autofs so as to keep autofs useable in
    the general case. This patch removes dependency on smbfs which
    is only useable for users of samba.

--- recipes/default.rb
+++ recipes/default.rb
@@ -9,12 +9,7 @@
 
 package "autofs"
 
-# Install smbfs package, unless we are on ubuntu 13.x
-package platform?("ubuntu") && node['platform_version'].to_f >= 12.10 ? "cifs-utils" : "smbfs"
-
 service "autofs" do
 	supports [ :start, :stop, :restart, :reload, :status ]
 	action [ :enable, :start ]
 end
-
-
